b02a6a88c600c7bd4829db1d7456de49a29f8751
yangyang118 <yangyang118@baidu.com>
2025-11-23 15:02:43 +0800

    vitepress

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,5 @@
 import { makeInstaller } from "@element/utils";
 import { components } from "./components";
-
+import '@element/theme/index.css'
 export default makeInstaller(components);
 export * from "@element/components";
