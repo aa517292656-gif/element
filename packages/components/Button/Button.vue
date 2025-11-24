056db1f99909d15c5562c99b1d3a4384ab96a393
yangyang118 <yangyang118@baidu.com>
2025-11-24 19:38:09 +0800

    button

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,13 +1,47 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import type { ButtonProps } from './type'
+import './style.less'
 defineOptions({
   name: 'ElButton'
 })
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: 'default',
+  size: 'default',
+  nativeType: 'button',
+  loading: false,
+  disabled: false,
+  tag: 'button',
+  plain: false,
+  round: false,
+  circle: false,
+  link: false
+})
 
-const handleClick = () => {
-  console.log('click')
-}
+const slots = defineSlots()
+
+const _ref = ref<HTMLButtonElement>()
 </script>
 
 <template>
-  <button @click="handleClick">Button</button>
+  <component 
+    :is="props.tag" 
+    ref="_ref" 
+    :type="tag === 'button' ? nativeType : 'button'" 
+    class="el-button"
+    :disabled="disabled || loading" 
+    :class="
+    {
+      [`el-button--${type}`]: type,
+      [`el-button--${size}`]: size,
+      'is-loading': loading,
+      'is-disabled': disabled,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-link': link,
+    }"
+    >
+    <slot></slot>
+  </component>
 </template>
